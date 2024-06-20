afafb0bb10e0fa6fc38dbd830ba6512a6e6bfef8
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-06-21 04:44:54 +0530

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,15 @@ import { ModeToggle } from "./themeToggle";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { doLogout } from "@/lib/actions";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut } from "lucide-react";
 
 const Navbar = () => {
     const { data: session, status } = useSession();
@@ -35,12 +44,32 @@ const Navbar = () => {
                     <div className="flex flex-row items-center gap-4 ">
                         <ModeToggle />
                         {session ? (
-                            <div
-                                onClick={doLogout}
-                                className="px-4 py-2 font-light text-white bg-blue-600 rounded-lg cursor-pointer hover:bg-blue-400"
-                            >
-                                Log Out
-                            </div>
+                            // <div
+                            //     onClick={doLogout}
+                            //     className="px-4 py-2 font-light text-white bg-blue-600 rounded-lg cursor-pointer hover:bg-blue-400"
+                            // >
+                            //     Log Out
+                            // </div>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger className="px-4 py-2 font-light text-white bg-blue-600 rounded-lg cursor-pointer hover:bg-blue-400">
+                                    Profile
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent className="bg-zinc-800">
+                                    <DropdownMenuLabel>
+                                        My Account
+                                    </DropdownMenuLabel>
+                                    <DropdownMenuSeparator />
+                                    <DropdownMenuItem>
+                                        <Link href={"/dashboard"}>
+                                            Dashboard
+                                        </Link>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem onClick={doLogout}>
+                                        <LogOut className="w-4 h-4 mr-2" />
+                                        <span>Log out</span>
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
                         ) : (
                             <Link
                                 href={"/login"}
